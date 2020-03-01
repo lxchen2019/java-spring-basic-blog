24284d93d9a938177da23e9df2075f1ae599e244
Lixiong Chen <lixiong.chen@capgemini.com>
2020-03-01 01:12:45 +0100

    module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,31 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/")
-    public String listPosts(ModelMap modelMap) {
-        modelMap.put("title", "Blog Post 1");
-        return "home";
+    public List<Post> listPosts(ModelMap modelMap) {
+        // modelMap.put("title", "Blog Post 1");
+
+        List<Post> allPosts = postRepository.getAllPosts();
+
+        modelMap.put("posts", allPosts);
+        return allPosts;
     }
 }
